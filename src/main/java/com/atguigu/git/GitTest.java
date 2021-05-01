be01d6150415a63242b9fda489affaa28002ebd5
hy <hy@163.com>
2021-05-01 20:31:56 +0800

    pull commit44 in IDEA

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hot_fix2");
         System.out.println("push test!");
         System.out.println("pull test22!");
+        System.out.println("pull test44!");
     }
 }
