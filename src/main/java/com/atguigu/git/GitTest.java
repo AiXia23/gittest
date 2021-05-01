c64dba587b7f460d0baf13c519cb9a6ec83df1fb
hy <hy@163.com>
2021-05-01 16:53:36 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author huy
+ * @create 2021-05-01 16:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
