da222dd07ce72bb82f7831b9136e531956fb1c6a
AiXia23 <64726863+AiXia23@users.noreply.github.com>
2021-05-01 19:47:16 +0800

    pull test in github!

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("master commit2");
         System.out.println("hot_fix2");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
